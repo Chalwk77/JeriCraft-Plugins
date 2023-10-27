d22e1a73dfb9da1d93d3d96879ecb4b99c9c2bcd
Chalwk77 <jericho.crosby227@gmail.com>
2023-10-28 10:46:53 +1300

    ...

--- TacticalInsertion/README.md
+++
@@ -1,3 +0,0 @@
-# Tactical Insertion (TacInsert)
-
-Welcome to Tac Insert, a Slimefun4 addon that adds spawn beacons to your server!
--- TacticalInsertion/src/main/java/com/chalwk/TacticalInsertion.java
+++ TacticalInsertion/src/main/java/com/chalwk/TacticalInsertion.java
@@ -1,17 +1,17 @@
 package com.chalwk;
 
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
-
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
-import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import io.github.thebusybiscuit.slimefun4.api.researches.Research;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class TacticalInsertion extends JavaPlugin implements SlimefunAddon {
 
@@ -21,19 +21,37 @@ public class TacticalInsertion extends JavaPlugin implements SlimefunAddon {
         Config cfg = new Config(this);
 
         if (cfg.getBoolean("options.auto-update")) {
-
+            //
+            // for a future update
+            //
         }
 
         ItemStack itemGroupItem = new CustomItemStack(Material.DIAMOND, "&4Test Category", "", "&a> Click to open");
-
         NamespacedKey itemGroupId = new NamespacedKey(this, "test_category");
         ItemGroup itemGroup = new ItemGroup(itemGroupId, itemGroupItem);
 
         SlimefunItemStack slimefunItem = new SlimefunItemStack("COOL_DIAMOND", Material.DIAMOND, "&4Cool Diamond", "&c+20% Coolness");
-        ItemStack[] recipe = { new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.DIAMOND), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD) };
-
-        SlimefunItem item = new SlimefunItem(itemGroup, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
+        SlimefunItem item = getSlimefunItem(itemGroup, slimefunItem);
         item.register(this);
+
+        NamespacedKey researchKey = new NamespacedKey(this, "test_research");
+        Research research = new Research(researchKey, 4825, "test_message", 10);
+        research.addItems(item);
+        research.register();
+    }
+
+    private SlimefunItem getSlimefunItem(ItemGroup itemGroup, SlimefunItemStack slimefunItem) {
+        ItemStack[] recipe = {
+                new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD),
+                null, new ItemStack(Material.DIAMOND), null,
+                new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD)};
+
+        /* RECIPE LAYOUT:
+            [E] [ ] [E]
+            [ ] [D] [ ]
+            [E] [ ] [E]
+        */
+        return new SlimefunItem(itemGroup, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
     }
 
     @Override
